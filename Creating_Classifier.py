b590e33d061594eece9b3a887a11d8a57ae341fa
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-07-25 16:42:59 +0530

    Add files via upload

--- Creating_Classifier.py
+++ Creating_Classifier.py
@@ -20,6 +20,10 @@ import datetime
 
 start_time = time.time()
 
+
+# importing the multiprocessing module 
+import multiprocessing 
+import os
 import pandas as pd
 import numpy as np
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
@@ -34,89 +38,106 @@ from textblob import classifiers
 def split_into_parts(number, n_parts):
     a_1 = np.array([0.0,1710.0])
     a = np.around((np.linspace(0, number, n_parts+1)[1:]))
+    b = np.append(a,a_1)
     b = np.reshape(a, (-1, 2))
-    #b = np.concatenate(b,a_1)
-    print(b)
     for i in range(len(b)):
         b[i][0] = b[i][0] +1
         print(b[i][0])
     b = np.sort(b,axis=0)
     return b
 
-broken_number_of_parts = split_into_parts(54716,32)
-print(broken_number_of_parts[0])
+#broken_number_of_parts = split_into_parts(54716,32)
+#print(broken_number_of_parts[-len(broken_number_of_parts)])
 #broken_number_of_parts_2D = np.reshape(broken_number_of_parts, (-1, 2))
  
 
-
-df_for_raw = pd.DataFrame(columns=['sentences','polarity'])
-pos = 0
-neg = 0
-polarity_list = []
-sentncs_list = []
-
-#for i in range(len(reuters.sents())):
-for i in range(10000):    
-    sentncs = " ".join(reuters.sents()[i])
-    #print("sentncs = ", sentncs)
-    blob = TextBlob(sentncs)
-    sentncs_list.append(sentncs)
-    if blob.sentiment.polarity > 0:
-        polarity_list.append('pos')
-        pos = pos + 1
-    elif blob.sentiment.polarity < 0:
-        polarity_list.append('neg')
-        neg = neg + 1
-'''
-for i in range(len(reutersDf)):
-    #print(type(reutersDf['text'][i]))
-    #print(reutersDf['text'][i])
-    #blob = TextBlob(reutersDf['text'][i])
-    text_token = sent_tokenize(reutersDf['text'][i])
-    for j in range(len(text_token)):
-        sentncs = text_token[j]
-        sentncs_list.append(sentncs)
+def create_raw_data_for_classifier(start_pt,end_pt,return_raw_data):
+    # printing process id 
+    #print("ID of process running : {}".format(os.getpid()))
+    
+    #df_for_raw = pd.DataFrame(columns=['sentences','polarity'])
+    pos = 0
+    neg = 0
+    polarity_list = []
+    sentncs_list = []
+    
+    #for i in range(len(reuters.sents())):
+    for i in range(start_pt,end_pt):    
+        sentncs = " ".join(reuters.sents()[i])
+        #print("sentncs = ", sentncs)
         blob = TextBlob(sentncs)
+        sentncs_list.append(sentncs)
         if blob.sentiment.polarity > 0:
-            #print('Positive: ', round(blob.sentiment.polarity,2))
-            #reutersDf['polarity'].loc[ reutersDf.text == reutersDf['text'][i] ] = 'pos'
             polarity_list.append('pos')
             pos = pos + 1
         elif blob.sentiment.polarity < 0:
-            #print('Negative: ', round(blob.sentiment.polarity,2))
-            #reutersDf['polarity'].loc[ reutersDf.text == reutersDf['text'][i] ] = 'neg'
             polarity_list.append('neg')
             neg = neg + 1
-'''
-raw_data = list(zip(sentncs_list,polarity_list))
-#print(reutersDf.tail(10))
-print("raw_data len = ",len(raw_data))
-print("Total pos = ",pos," Total Neg =",neg)
-print(raw_data[0])
-#text_list = reutersDf['text'].values.tolist()
-#polarity_list = reutersDf['polarity'].values.tolist()
+    
+    raw_data = list(zip(sentncs_list,polarity_list))
+    #print(reutersDf.tail(10))
+    print("raw_data len = ",len(raw_data))
+    print("Total pos = ",pos," Total Neg =",neg)
+    print(raw_data[0])
+    return_raw_data = raw_data
 
-#raw_data_for_classifier = list(zip(text_list,polarity_list))
-#print("raw_data_for_classifier = ",raw_data_for_classifier)
-
-np.random.shuffle(raw_data)
-training = raw_data[:5000]
-testing = raw_data[-5000:]
-
-classifier = classifiers.NaiveBayesClassifier(training)
-
-## decision tree classifier
-dt_classifier = classifiers.DecisionTreeClassifier(training)
-
-print ("classifier.accuracy = ",classifier.accuracy(testing))
-print ("dt_classifier.accuracy = ",dt_classifier.accuracy(testing))
+def get_the_classifier_accuracy(raw_data,return_dt_classifier_accuracy,return_NaiveBayesClassifier):
+    np.random.shuffle(raw_data)
+    training = raw_data[:5000]
+    testing = raw_data[-5000:]
+    
+    classifier = classifiers.NaiveBayesClassifier(training)
+    
+    ## decision tree classifier
+    dt_classifier = classifiers.DecisionTreeClassifier(training)
+    
+    print ("classifier.accuracy = ",classifier.accuracy(testing))
+    print ("dt_classifier.accuracy = ",dt_classifier.accuracy(testing))
+    return_dt_classifier_accuracy = dt_classifier
+    return_NaiveBayesClassifier = classifier
 
 def convert(n): 
 	return str(datetime.timedelta(seconds = n)) 
 	
-n =  time.time() - start_time
+
 
 def convert_sec(n): 
     return str(datetime.timedelta(seconds = n))
 
-print("---Execution Time ---",convert_sec(n))
+if __name__ == "__main__": 
+    manager = multiprocessing.Manager()
+    print("ID of main process: {}".format(os.getpid()))
+    return_raw_data = manager.list()
+    return_dt_classifier_accuracy = manager.list()
+    return_NaiveBayesClassifier = manager.list()
+    # creating processes 
+    p1 = multiprocessing.Process(target=create_raw_data_for_classifier,args=(0,100,return_raw_data))    
+    # starting process 1 
+    p1.start() 
+    # wait until process 1 is finished 
+    
+    # process IDs 
+    print("ID of process p1: {}".format(p1.pid))
+    
+    p1.join() 
+    # both processes finished 
+    print("process has finished execution!!") 
+    
+    # check if processes are alive 
+    print("Process p1 is alive: {}".format(p1.is_alive()))
+    print(p1)
+    print("Len of return_raw_data",len(return_raw_data))
+    p2 = multiprocessing.Process(target=get_the_classifier_accuracy,args=(return_raw_data,return_dt_classifier_accuracy,return_NaiveBayesClassifier))    
+    
+    p2.start() 
+    
+    # process IDs 
+    print("ID of process p2: {}".format(p2.pid))
+    
+    p2.join()
+    print("return_dt_classifier_accuracy = ",return_dt_classifier_accuracy)
+    print("return_NaiveBayesClassifier = ",return_NaiveBayesClassifier)
+    # check if processes are alive 
+    print("Process p2 is alive: {}".format(p2.is_alive()))
+    n =  time.time() - start_time
+    print("---Execution Time ---",convert_sec(n))
