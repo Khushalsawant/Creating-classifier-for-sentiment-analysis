aeeb79fdd564a59fc2059d196b4de356d5bc1f53
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-07-25 19:01:45 +0530

    Add files via upload

--- Creating_Classifier.py
+++ Creating_Classifier.py
@@ -48,42 +48,7 @@ def split_into_parts(number, n_parts):
     b = np.sort(b,axis=0)
     return b
 
-#broken_number_of_parts = split_into_parts(54716,32)
-#print(broken_number_of_parts[-len(broken_number_of_parts)])
-#broken_number_of_parts_2D = np.reshape(broken_number_of_parts, (-1, 2))
- 
-
-def create_raw_data_for_classifier(start_pt,end_pt,return_raw_data):
-    # printing process id 
-    #print("ID of process running : {}".format(os.getpid()))
-    
-    #df_for_raw = pd.DataFrame(columns=['sentences','polarity'])
-    pos = 0
-    neg = 0
-    polarity_list = []
-    sentncs_list = []
-    
-    #for i in range(len(reuters.sents())):
-    for i in range(start_pt,end_pt):    
-        sentncs = " ".join(reuters.sents()[i])
-        #print("sentncs = ", sentncs)
-        blob = TextBlob(sentncs)
-        sentncs_list.append(sentncs)
-        if blob.sentiment.polarity > 0:
-            polarity_list.append('pos')
-            pos = pos + 1
-        elif blob.sentiment.polarity < 0:
-            polarity_list.append('neg')
-            neg = neg + 1
-    
-    raw_data = list(zip(sentncs_list,polarity_list))
-    #print(reutersDf.tail(10))
-    print("raw_data len = ",len(raw_data))
-    print("Total pos = ",pos," Total Neg =",neg)
-    print(raw_data[0])
-    return_raw_data = raw_data
-
-def create_raw_data_for_classifier1(start_pt,end_pt):
+def create_raw_data_for_classifier(start_pt,end_pt):
     # printing process id 
     #print("ID of process running : {}".format(os.getpid()))
     
@@ -119,7 +84,7 @@ def create_raw_data_for_classifier1(start_pt,end_pt):
     return raw_data
 
 
-def get_the_classifier_accuracy(raw_data,return_dt_classifier_accuracy,return_NaiveBayesClassifier):
+def get_the_classifier_accuracy(raw_data):
     np.random.shuffle(raw_data)
     training = raw_data[:5000]
     testing = raw_data[-5000:]
@@ -128,31 +93,15 @@ def get_the_classifier_accuracy(raw_data,return_dt_classifier_accuracy,return_Na
     
     ## decision tree classifier
     dt_classifier = classifiers.DecisionTreeClassifier(training)
-    
+    NaiveBayesClassifier_accuracy = classifier.accuracy(testing)
+    DecisionTreeClassifier_accuracy = dt_classifier.accuracy(testing)
     print ("classifier.accuracy = ",classifier.accuracy(testing))
     print ("dt_classifier.accuracy = ",dt_classifier.accuracy(testing))
-    return_dt_classifier_accuracy = dt_classifier
-    return_NaiveBayesClassifier = classifier
-
-
-def get_the_classifier_accuracy1(raw_data):
-    np.random.shuffle(raw_data)
-    training = raw_data[:5000]
-    testing = raw_data[-5000:]
-    
-    classifier = classifiers.NaiveBayesClassifier(training)
-    
-    ## decision tree classifier
-    dt_classifier = classifiers.DecisionTreeClassifier(training)
-    
-    print ("classifier.accuracy = ",classifier.accuracy(testing))
-    print ("dt_classifier.accuracy = ",dt_classifier.accuracy(testing))
-    return dt_classifier,classifier
+    return NaiveBayesClassifier_accuracy,DecisionTreeClassifier_accuracy
 
 def convert(n): 
 	return str(datetime.timedelta(seconds = n)) 
 	
-
 
 def convert_sec(n): 
     return str(datetime.timedelta(seconds = n))
@@ -201,9 +150,11 @@ if __name__ == "__main__":
     print("Process p2 is alive: {}".format(p2.is_alive()))
     '''
     pool = multiprocessing.Pool(processes=4)
-    result_list = pool.starmap(create_raw_data_for_classifier1,[(0,100)])#product([(0,100)],repeat=2))
+    split_into_parts = pool.starmap(split_into_parts,[(54716,32)])
+    print("split_into_parts = ",split_into_parts)
+    result_list = pool.starmap(create_raw_data_for_classifier,[(0,3000)])#product([(0,100)],repeat=2))
     #print(result_list)
-    result_accuracy1 = pool.starmap(get_the_classifier_accuracy1,[result_list])#product([(0,100)],repeat=2))
+    result_accuracy1 = pool.starmap(get_the_classifier_accuracy,[result_list])#product([(0,100)],repeat=2))
     print("result_accuracy1 = ",result_accuracy1)
     pool.close()
     pool.join()
