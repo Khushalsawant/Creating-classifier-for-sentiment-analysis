4be1cf9ecd7d085e825699a19c6af9430287f938
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-07-25 18:46:38 +0530

    Add files via upload

--- Creating_Classifier.py
+++ Creating_Classifier.py
@@ -24,6 +24,8 @@ start_time = time.time()
 # importing the multiprocessing module 
 import multiprocessing 
 import os
+from itertools import product
+
 import pandas as pd
 import numpy as np
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
@@ -81,6 +83,42 @@ def create_raw_data_for_classifier(start_pt,end_pt,return_raw_data):
     print(raw_data[0])
     return_raw_data = raw_data
 
+def create_raw_data_for_classifier1(start_pt,end_pt):
+    # printing process id 
+    #print("ID of process running : {}".format(os.getpid()))
+    
+    #df_for_raw = pd.DataFrame(columns=['sentences','polarity'])
+    pos = 0
+    neg = 0
+    polarity_list = []
+    sentncs_list = []
+    
+    #for i in range(len(reuters.sents())):
+    for i in range(start_pt,end_pt):    
+        sentncs = " ".join(reuters.sents()[i])
+        #print("sentncs = ", sentncs)
+        blob = TextBlob(sentncs)
+        sentncs_list.append(sentncs)
+        if blob.sentiment.polarity > 0:
+            polarity_list.append('pos')
+            pos = pos + 1
+        elif blob.sentiment.polarity < 0:
+            polarity_list.append('neg')
+            neg = neg + 1
+    raw_data = list(zip(sentncs_list,polarity_list))
+    #print(reutersDf.tail(10))
+    print("raw_data len = ",len(raw_data))
+    print("Total pos = ",pos," Total Neg =",neg)
+    print(raw_data[0])
+    
+    raw_data = list(zip(sentncs_list,polarity_list))
+    #print(reutersDf.tail(10))
+    print("raw_data len = ",len(raw_data))
+    print("Total pos = ",pos," Total Neg =",neg)
+    print(raw_data[0])
+    return raw_data
+
+
 def get_the_classifier_accuracy(raw_data,return_dt_classifier_accuracy,return_NaiveBayesClassifier):
     np.random.shuffle(raw_data)
     training = raw_data[:5000]
@@ -95,6 +133,21 @@ def get_the_classifier_accuracy(raw_data,return_dt_classifier_accuracy,return_Na
     print ("dt_classifier.accuracy = ",dt_classifier.accuracy(testing))
     return_dt_classifier_accuracy = dt_classifier
     return_NaiveBayesClassifier = classifier
+
+
+def get_the_classifier_accuracy1(raw_data):
+    np.random.shuffle(raw_data)
+    training = raw_data[:5000]
+    testing = raw_data[-5000:]
+    
+    classifier = classifiers.NaiveBayesClassifier(training)
+    
+    ## decision tree classifier
+    dt_classifier = classifiers.DecisionTreeClassifier(training)
+    
+    print ("classifier.accuracy = ",classifier.accuracy(testing))
+    print ("dt_classifier.accuracy = ",dt_classifier.accuracy(testing))
+    return dt_classifier,classifier
 
 def convert(n): 
 	return str(datetime.timedelta(seconds = n)) 
@@ -125,8 +178,11 @@ if __name__ == "__main__":
     
     # check if processes are alive 
     print("Process p1 is alive: {}".format(p1.is_alive()))
+    
     print(p1)
+    
     print("Len of return_raw_data",len(return_raw_data))
+    
     p2 = multiprocessing.Process(target=get_the_classifier_accuracy,args=(return_raw_data,return_dt_classifier_accuracy,return_NaiveBayesClassifier))    
     
     p2.start() 
@@ -135,9 +191,20 @@ if __name__ == "__main__":
     print("ID of process p2: {}".format(p2.pid))
     
     p2.join()
+    
     print("return_dt_classifier_accuracy = ",return_dt_classifier_accuracy)
+    
     print("return_NaiveBayesClassifier = ",return_NaiveBayesClassifier)
+    
     # check if processes are alive 
     print("Process p2 is alive: {}".format(p2.is_alive()))
+    
+    pool = multiprocessing.Pool(processes=4)
+    result_list = pool.starmap(create_raw_data_for_classifier1,[(0,100)])#product([(0,100)],repeat=2))
+    #print(result_list)
+    result_accuracy1 = pool.starmap(get_the_classifier_accuracy1,[result_list])#product([(0,100)],repeat=2))
+    print( result_accuracy1)
+    
     n =  time.time() - start_time
+    
     print("---Execution Time ---",convert_sec(n))
